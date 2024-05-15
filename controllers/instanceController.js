46c996e7b269c0c9bb82f36057b4e1dd60718635
Veregorn <rjbarco@gmail.com>
2024-05-15 10:14:56 +0200

    New race instances form created

--- controllers/instanceController.js
+++ controllers/instanceController.js
@@ -6,6 +6,7 @@ const Race = require('../models/race');
 
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
+const race = require('../models/race');
 
 // Display the home page.
 exports.index = asyncHandler(async (req, res) => {
@@ -81,15 +82,79 @@ exports.instance_detail = asyncHandler(async (req, res, next) => {
 });
 
 // Display Instance create form on GET.
-exports.instance_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Instance create GET');
-};
+exports.instance_create_get = asyncHandler(async (req, res) => {
+    // Get all the modalities available to choose from and populate the name of every race.
+    const modalities = await Modality.find().sort({ distance: 1 }).populate('race').exec();
+
+    res.render('instance_form', {
+        title: 'Create Instance',
+        instance: null,
+        modalities: modalities,
+        errors: null,
+        layout: 'layout',
+    });
+});
 
 // Handle Instance create on POST.
-exports.instance_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Instance create POST');
-};
+exports.instance_create_post = [
+    // Validate and sanitize fields.
+    body('modality', 'Modality must be specified')
+        .isLength({ min: 1 })
+        .trim()
+        .escape(),
+    body('date', 'Date must be specified')
+        .isISO8601()
+        .toDate(),
+    body('price', 'Price must be specified and greater than or equal to 0')
+        .isNumeric()
+        .toFloat()
+        .isFloat({ min: 0 })
+        .escape(),
 
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create an instance object with escaped and trimmed data.
+        const instance = new Instance({
+            modality: req.body.modality,
+            date: req.body.date,
+            price: req.body.price,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+            
+            // Get all the modalities available to choose from and populate the name of every race.
+            const modalities = await Modality.find().sort({ distance: 1 }).populate('race').exec();
+
+            res.render('instance_form', {
+                title: 'Create Instance',
+                instance: instance,
+                modalities: modalities,
+                errors: errors.array(),
+                layout: 'layout',
+            });
+            return;
+        }
+
+        try {
+            // Check if an instance with the same modality and date already exists.
+            const foundInstance = await Instance.findOne({ modality: instance.modality, date: instance.date }).exec();
+            if (foundInstance) {
+                res.redirect(foundInstance.url);
+            } else {
+                // Save the instance.
+                await instance.save();
+                res.redirect(instance.url);
+            }
+        } catch (error) {
+            return next(error);
+        }
+    }),
+];
+    
 // Display Instance delete form on GET.
 exports.instance_delete_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Instance delete GET');
